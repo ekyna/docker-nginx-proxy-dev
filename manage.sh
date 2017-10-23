aa30e54a01e8e1b245e3c47fabb157ca83d9e2e7
Etienne Dauvergne <contact@ekyna.com>
2017-10-23 18:56:28 +0200

    Backup and restore connected network list.

--- manage.sh
+++ manage.sh
@@ -11,17 +11,37 @@ LOG_PATH="$DIR/docker_logs.txt"
 
 source ./.env
 
+if [[ "" == "${COMPOSE_PROJECT_NAME}" ]]; then printf "\e[31mCOMPOSE_PROJECT_NAME env variable is not set.\e[0m\n"; exit; fi
+
+# Clear logs
+echo "" > ${LOG_PATH}
+
 IsUpAndRunning() {
-    if [[ "$(docker ps | grep $1)" ]]
+    if [[ "$(docker ps | grep ${COMPOSE_PROJECT_NAME}_$1)" ]]
     then
-        return 1
+        return 0
     fi
-    return 0
+    return 1
+}
+
+CheckProxyUpAndRunning() {
+    if ! IsUpAndRunning nginx
+    then
+        printf "\e[31mProxy is not up and running.\e[0m\n"
+        exit 1
+    fi
+}
+
+NetworkExists() {
+    if [[ "$(docker network ls | grep $1)" ]]
+    then
+        return 0
+    fi
+    return 1
 }
 
 ComposeUp() {
-    IsUpAndRunning "${COMPOSE_PROJECT_NAME}_nginx"
-    if [[ $? -eq 1 ]]
+    if IsUpAndRunning nginx
     then
         printf "\e[31mAlready up and running.\e[0m\n"
         exit 1
@@ -32,6 +52,19 @@ ComposeUp() {
         docker-compose up -d >> ${LOG_PATH} 2>&1 \
             && printf "\e[32mdone\e[0m\n" \
             || (printf "\e[31merror\e[0m\n" && exit 1)
+
+    if [[ -f ./networks.list ]]
+    then
+        while IFS='' read -r NETWORK || [[ -n "$NETWORK" ]]; do
+            if [[ "" != "${NETWORK}" ]]
+            then
+                Connect ${NETWORK}
+            fi
+        done < ./networks.list
+
+        sleep 1
+        docker restart ${COMPOSE_PROJECT_NAME}_nginx
+    fi
 }
 
 ComposeDown() {
@@ -43,12 +76,7 @@ ComposeDown() {
 }
 
 Execute() {
-    IsUpAndRunning "${COMPOSE_PROJECT_NAME}_nginx"
-    if [[ $? -eq 0 ]]
-    then
-        printf "\e[31mNot up and running.\e[0m\n"
-        exit 1
-    fi
+    CheckProxyUpAndRunning
 
     printf "Executing $1\n"
 
@@ -73,14 +101,29 @@ GenCert() {
 }
 
 Connect() {
-    if [[ "$(docker network ls | grep $1)" == "" ]]
+    CheckProxyUpAndRunning
+
+    NETWORK="$(echo -e "$1" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+
+    if ! NetworkExists ${NETWORK}
     then
-        printf "\e[31mNetwork '$1' does not exist.\e[0m\n"
+        printf "\e[31mNetwork '${NETWORK}' does not exist.\e[0m\n"
         exit
     fi
 
-    docker network connect $1 ${COMPOSE_PROJECT_NAME}_nginx
-    docker network connect $1 ${COMPOSE_PROJECT_NAME}_generator
+    printf "Connecting to \e[1;33m${NETWORK}\e[0m network ... "
+
+    docker network connect ${NETWORK} ${COMPOSE_PROJECT_NAME}_nginx >> ${LOG_PATH} 2>&1 || (printf "\e[31merror\e[0m\n" && exit 1)
+    docker network connect ${NETWORK} ${COMPOSE_PROJECT_NAME}_generator >> ${LOG_PATH} 2>&1 || (printf "\e[31merror\e[0m\n" && exit 1)
+
+    printf "\e[32mdone\e[0m\n"
+
+    if [[ -f ./networks.list ]];
+    then
+        if [[ "$(cat ./networks.list | grep ${NETWORK})" ]]; then return 0; fi
+    fi
+
+    echo $1 >> ./networks.list
 }
 
 # ----------------------------- EXEC -----------------------------
