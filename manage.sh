9a14bf8b5ebfd833f403cd7e1c27a6b6f93fd984
Etienne Dauvergne <contact@ekyna.com>
2021-09-08 20:02:36 +0200

    Use new proxy images.

--- manage.sh
+++ manage.sh
@@ -24,19 +24,19 @@ if [[ -z ${COMPOSE_PROJECT_NAME+x} ]]; then printf "\e[31mThe 'COMPOSE_PROJECT_N
 echo "" > ${LOG_PATH}
 
 Success() {
-    printf "\e[32m$1\e[0m\n"
+    printf "\e[32m%s\e[0m\n" "$1"
 }
 
 Error() {
-    printf "\e[31m$1\e[0m\n"
+    printf "\e[31m%s\e[0m\n" "$1"
 }
 
 Warning() {
-    printf "\n\e[31;43m$1\e[0m\n"
+    printf "\e[31;43m%s\e[0m\n" "$1"
 }
 
 Help() {
-    printf "\n\e[2m$1\e[0m\n";
+    printf "\e[2m%s\e[0m\n" "$1"
 }
 
 DoneOrError() {
@@ -55,8 +55,8 @@ Confirm () {
     while [[ "$choice" != "n" ]] && [[ "$choice" != "y" ]]
     do
         printf "Do you want to continue ? (N/Y)"
-        read choice
-        choice=$(echo ${choice} | tr '[:upper:]' '[:lower:]')
+        read -r choice
+        choice=$(echo "${choice}" | tr '[:upper:]' '[:lower:]')
     done
     if [[ "$choice" = "n" ]]; then
         printf "\nAbort by user.\n"
@@ -66,7 +66,7 @@ Confirm () {
 }
 
 IsUpAndRunning() {
-    if [[ "$(docker ps --format '{{.Names}}' | grep ${COMPOSE_PROJECT_NAME}_$1\$)" ]]
+    if docker ps --format '{{.Names}}' | grep -q "$1\$"
     then
         return 0
     fi
@@ -74,7 +74,7 @@ IsUpAndRunning() {
 }
 
 CheckProxyUpAndRunning() {
-    if ! IsUpAndRunning nginx
+    if ! IsUpAndRunning proxy_nginx
     then
         Error "Proxy is not up and running."
         exit 1
@@ -82,7 +82,7 @@ CheckProxyUpAndRunning() {
 }
 
 NetworkExists() {
-    if [[ "$(docker network ls | grep $1)" ]]
+    if docker network ls --format '{{.Name}}' | grep -q "$1\$"
     then
         return 0
     fi
@@ -90,7 +90,7 @@ NetworkExists() {
 }
 
 ComposeUp() {
-    if IsUpAndRunning nginx
+    if IsUpAndRunning proxy_nginx
     then
         Error "Already up and running."
         exit 1
@@ -105,12 +105,12 @@ ComposeUp() {
         while IFS='' read -r NETWORK || [[ -n "$NETWORK" ]]; do
             if [[ "" != "${NETWORK}" ]]
             then
-                Connect ${NETWORK}
+                Connect "${NETWORK}"
             fi
         done < ./networks.list
 
         sleep 1
-        docker restart ${COMPOSE_PROJECT_NAME}_nginx
+        docker restart proxy_nginx
     fi
 }
 
@@ -123,14 +123,15 @@ ComposeDown() {
 Execute() {
     CheckProxyUpAndRunning
 
-    printf "Executing $1\n"
+    printf "Executing %s\n" "$1"
 
     printf "\n"
+    # shellcheck disable=SC1001
     if [[ "$(uname -s)" = \MINGW* ]]
     then
-        winpty docker exec -it ${COMPOSE_PROJECT_NAME}_nginx $1
+        winpty docker exec -it proxy_nginx $1
     else
-        docker exec -it ${COMPOSE_PROJECT_NAME}_nginx $1
+        docker exec -it proxy_nginx $1
     fi
     printf "\n"
 }
@@ -150,22 +151,22 @@ Connect() {
 
     NETWORK="$(echo -e "$1" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
-    if ! NetworkExists ${NETWORK}
+    if ! NetworkExists "${NETWORK}"
     then
-        printf "\e[31mNetwork '${NETWORK}' does not exist.\e[0m\n"
+        Error "Network '${NETWORK}' does not exist."
         exit
     fi
 
-    printf "Connecting to \e[1;33m${NETWORK}\e[0m network ... "
+    printf "Connecting to \e[1;33m%s\e[0m network ... " "${NETWORK}"
 
-    docker network connect ${NETWORK} ${COMPOSE_PROJECT_NAME}_nginx >> ${LOG_PATH} 2>&1
+    docker network connect "${NETWORK}" proxy_nginx >> ${LOG_PATH} 2>&1
     if [[ $? -ne 0 ]]
     then
         Error 'error'
         exit 1
     fi
 
-    docker network connect ${NETWORK} ${COMPOSE_PROJECT_NAME}_generator >> ${LOG_PATH} 2>&1
+    docker network connect ${NETWORK} proxy_generator >> ${LOG_PATH} 2>&1
     if [[ $? -ne 0 ]]
     then
         Error 'error'
@@ -216,19 +217,19 @@ case $1 in
         Connect $2
     ;;
     restart)
-        if ! IsUpAndRunning nginx
+        if ! IsUpAndRunning proxy_nginx
         then
             printf "\e[31mNot up and running.\e[0m\n"
             exit 1
         fi
 
-        docker restart ${COMPOSE_PROJECT_NAME}_nginx
+        docker restart proxy_nginx
     ;;
     dump)
-        cat ./volumes/conf.d/default.conf
+        docker exec -t proxy_nginx cat /etc/nginx/conf.d/default.conf
     ;;
     test)
-        docker exec ${COMPOSE_PROJECT_NAME}_nginx nginx -t
+        docker exec proxy_nginx nginx -t
     ;;
     reset)
         Warning "Configured networks and certificates will be lost."
@@ -238,17 +239,17 @@ case $1 in
         Reset
     ;;
     *)
-        Help "Usage:  ./manage.sh [action] [options]
+        printf "\e[2mUsage:  ./manage.sh [action] [options]
 
- - \e[0mup\e[2m\t\t Create and start containers for the [env] environment.
- - \e[0mdown\e[2m\t\t Stop and remove containers for the [env] environment.
- - \e[0mgencert\e[2m name\t Generates certificates for [name] domain.
- - \e[0mconnect\e[2m name\t Connects proxy to [name] network.
- - \e[restart\e[2m name\t Restart nginx container.
- - \e[0mdump\e[2m\t Dump nginx config.
- - \e[0mtest\e[2m\t Test nginx config.
- - \e[0mreset\e[2m\t Reset the containers and network connections.
-"
+  \e[0mup\e[2m             Create and start containers for the [env] environment.
+  \e[0mdown\e[2m           Stop and remove containers for the [env] environment.
+  \e[0mgencert\e[2m name   Generates certificates for [name] domain.
+  \e[0mconnect\e[2m name   Connects proxy to [name] network.
+  \e[0mrestart\e[2m        Restart nginx container.
+  \e[0mdump\e[2m           Dump nginx config.
+  \e[0mtest\e[2m           Test nginx config.
+  \e[0mreset\e[2m          Reset the containers and network connections.
+\e[0m"
     ;;
 esac
 
